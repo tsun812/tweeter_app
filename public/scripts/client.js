94d54b8671b542bfaa02532afc4ee8f38feb9e48
Yan Sun <tsun812@gmail.com>
2021-12-15 20:02:30 -0800

    display all tweet history on homepage once loaded, add new tweet on homepage once submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,56 +1,21 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1639437853423
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1639524253423
-  }
-]
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-$(document).ready(() => {
-  $button = $(".new-tweet, button");
-  $button.submit(function (event) {
-    event.preventDefault();
-    const data = $("new-tweet, form").serialize();
-    console.log(data);
-    $.post('http://localhost:8080/tweets/',  data)
-    .done(function() {
-      alert( "success");
-    })
-    .fail(function() {
-      alert( "error" );
-    })
-    });
+
+const loadtweets = function (){
+  $.get( "http://localhost:8080/tweets/", function( dataInput ) {
+    renderTweets(dataInput);
+    
+  });
+}
 
 const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  for(element of tweets){
-    const $curr = createTweetElement(element);
+  for(let i = tweets.length - 1; i >= 0; i--){
+    const $curr = createTweetElement(tweets[i]);
     $('.tweet-history').append($curr);
   }
-}
+  }
+
 
 const createTweetElement = function(tweetObj) {
   let $tweet = $(`<article>
@@ -76,6 +41,36 @@ const createTweetElement = function(tweetObj) {
   // ...
   return $tweet;
 }
+loadtweets();
+$(document).ready(() => {
+  
+  $button = $(".new-tweet, button");
+  $button.submit(function (event) {
+    event.preventDefault();
+    const data = $("new-tweet, form").serialize();
+    console.log(data);
+    const $text = $('textarea');
+    const textvalue = $text.val().split(" ").filter(function(str) {
+      return /\S/.test(str);
+    })
+  
+    $.post('http://localhost:8080/tweets/',  data)
+    .done(function() {
+      loadtweets();
+      location.reload();
+    })
+    .fail(function() {
+      if(textvalue.length === 0){
+      alert( "Can't be blank" );
+      } 
+      else if (textvalue.length > 5){
+        alert("must be 140 characters or less")
+      }
+    })
+  
+ });
+    
 
-renderTweets(data);
+
+
 })
